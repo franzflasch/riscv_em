17f250dbd18bd18e48bdd76469f8b848197be228
franzflasch <franz.flasch@gmx.at>
2023-06-25 08:18:47 +0200

    Update github_ci_run_tests.sh

--- github_ci_run_tests.sh
+++ github_ci_run_tests.sh
@@ -5,10 +5,12 @@ set -e
 ROOT_DIR=${PWD}
 DEFAULT_PATH=${PATH}
 
-TOOLCHAIN_DIR32=${ROOT_DIR}/toolchain32
+TOOLCHAIN_BASE_DIR=/opt/local
+
+TOOLCHAIN_DIR32=${TOOLCHAIN_BASE_DIR}/cross-tool-riscv32-baremetal-gcc13/
 cd ${ROOT_DIR}
 cd build32 && export PATH=${PWD}:${DEFAULT_PATH}
-cd ${TOOLCHAIN_DIR32}/toolchain/bin && export PATH=${PWD}:${PATH}
+cd ${TOOLCHAIN_DIR32}/bin && export PATH=${PWD}:${PATH}
 
 riscv32-none-elf-gcc --version
 
@@ -16,10 +18,10 @@ cd ${ROOT_DIR}/riscv-test-scripts
 ./build_riscv_em_tests.sh 32 ${ROOT_DIR}/dts/riscv_em.dtb
 
 
-TOOLCHAIN_DIR64=${ROOT_DIR}/toolchain64
+TOOLCHAIN_DIR64=${TOOLCHAIN_BASE_DIR}/cross-tool-riscv64-baremetal-gcc13/
 cd ${ROOT_DIR}
 cd build64 && export PATH=${PWD}:${DEFAULT_PATH}
-cd ${TOOLCHAIN_DIR64}/toolchain/bin && export PATH=${PWD}:${PATH}
+cd ${TOOLCHAIN_DIR64}/bin && export PATH=${PWD}:${PATH}
 
 riscv64-none-elf-gcc --version
 
